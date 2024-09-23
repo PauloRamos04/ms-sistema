0c024af0553275aa7d76752fd98f4c01878565f0
Paulo <pauloramos.04@hotmail.com>
2024-09-23 20:51:52 -0300

    first commit

---
+++ src/main/java/saudeconectada/fatec/ms_sistema/MsSistemaApplication.java
@@ -0,0 +1,13 @@
+package saudeconectada.fatec.ms_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saudeconectada/fatec/ms_sistema/MsSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package saudeconectada.fatec.ms_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
